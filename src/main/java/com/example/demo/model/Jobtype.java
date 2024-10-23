51b62434137f7ceb42dd8621df1066cae9865800
phuongnguyenthu <thuphuonggg135@gmail.com>
2024-10-23 14:26:29 +0700

    add change Jobtype

--- src/main/java/com/example/demo/model/Jobtype.java
+++ src/main/java/com/example/demo/model/Jobtype.java
@@ -1,4 +1,5 @@
 package com.example.demo.model;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.*;
@@ -7,20 +8,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name="job_type")
+@Table(name="jobtype")
 public class Jobtype {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String jobType_name;
+    private LocalDateTime create_at;
+    private LocalDateTime update_at;
 
+    @ManyToMany(mappedBy = "jobtypes")
+    private Set<Department> departments;
 
+    @OneToMany(mappedBy = "jobtype",cascade = CascadeType.ALL)
+    private Set<Task> tasks;
 
 
 }
