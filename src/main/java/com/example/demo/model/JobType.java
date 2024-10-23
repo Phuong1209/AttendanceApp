ac9a769dfc8caa172059e48ec3333d6ef2dadabb
PhuongPT <phamthanhp24@gmail.com>
2024-10-23 15:46:56 +0700

    update jobtype

--- src/main/java/com/example/demo/model/JobType.java
+++ src/main/java/com/example/demo/model/JobType.java
@@ -1,25 +1,30 @@
 package com.example.demo.model;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+import java.time.LocalDateTime;
+import java.util.Set;
 @Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name="job_type")
+@Table(name = "jobtype")
 public class JobType {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String jobType_name;
+    private LocalDateTime create_at;
+    private LocalDateTime update_at;
 
+    @ManyToMany(mappedBy = "jobtypes")
+    private Set<Department> departments;
 
-
-
+    @OneToMany(mappedBy = "jobtype",cascade = CascadeType.ALL)
+    private Set<Task> tasks;
 }
