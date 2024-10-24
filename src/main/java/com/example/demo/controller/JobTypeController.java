82f82c6e2f52597fdc67eb8c12d0f6082fe56296
HuongKumanthong <maihuong19j1@gmail.com>
2024-10-24 13:53:47 +0700

    commit Jobtype service + controller

---
+++ src/main/java/com/example/demo/controller/JobTypeController.java
@@ -0,0 +1,12 @@
+package com.example.demo.model.JobType;
+import com.model.JobType;
+import com.service.exam.IExamTestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+public class JobTypeController {
+}
---
+++ src/main/java/com/example/demo/service/IJobTypeServicve.java
@@ -0,0 +1,8 @@
+package com.service.JobType;
+
+import com.example.demo.model.JobType;
+//import com.example.demo.service.IJobTypeService;
+import org.springframework.stereotype.Service;
+
+public interface IJobTypeService extends IGeneralService<Test> {
+}
