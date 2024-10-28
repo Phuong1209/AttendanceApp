d53d1575101aa5fee293da2739e35b883865cd15
HuongKumanthong <maihuong19j1@gmail.com>
2024-10-28 11:20:07 +0700

    commit Jobtype service + controller+ repo

---
+++ src/main/java/com/example/demo/service/IJobTypeService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+//import com.model.JobType;
+//import com.model
+import org.springframework.stereotype.Service;
+
+public interface IJobTypeService extends
+}
--- src/main/java/com/example/demo/service/IJobTypeServicve.java
+++
@@ -1,8 +0,0 @@
-package com.service.JobType;
-
-import com.example.demo.model.JobType;
-//import com.example.demo.service.IJobTypeService;
-import org.springframework.stereotype.Service;
-
-public interface IJobTypeService extends IGeneralService<Test> {
-}
