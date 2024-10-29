43c9da2075f5ca54abb1d84aa5f8017d430337d5
SemeS01 <phamthanhp24@gmail.com>
2024-10-29 14:39:52 +0700

    update

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Department;
+import com.example.demo.model.dto.DepartmentDto;
 import com.example.demo.repository.DepartmentRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,13 +19,50 @@ import java.util.List;
 @RequestMapping("/department")
 
 public class DepartmentController {
+
+    //Show list Department
+    //1. Create Department Repo
     @Autowired
     private DepartmentRepository repo;
-
+    //2. Create method to show Department List
     @GetMapping({"","/"})
     public String showDepartmentList(Model model){
-        List<Department> departments = repo.findAll();
-        model.addAttribute("departments", departments);
-        return "department/index";
+        List<Department> departments = repo.findAll(); //add Department list from DB to "departments"
+        model.addAttribute("departments", departments); //add all department listed to model
+        return "department/index"; //show department from model to user view
     }
+
+    //Create Department
+    //1. Show Create form
+    @GetMapping("/create")
+    public String showCreatePage(Model model){
+        DepartmentDto departmentDto = new DepartmentDto(); //declare dto
+        model.addAttribute("departmentDto", departmentDto); //add dto to model
+        return "department/CreateDepartment";
+    }
+
+    //2. Create Department to DB
+    @PostMapping("/create")
+    public String createDepartment(
+            @Valid @ModelAttribute DepartmentDto departmentDto,
+            BindingResult result
+            ) {
+
+        if(result.hasErrors()){
+            return "department/CreateDepartment";
+        }
+
+        //create new department in db
+        Department department = new Department();
+        department.setName(departmentDto.getName());
+
+        repo.save(department);
+
+        return "redirect:/department";
+
+    }
+
+
+
+
 }
---
+++ src/main/java/com/example/demo/model/dto/DepartmentDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.model.dto;
+
+import jakarta.validation.constraints.*;
+
+public class DepartmentDto {
+    @NotEmpty(message = "The name is required")
+    private String name;
+
+    public @NotEmpty(message = "The name is required") String getName() {
+        return name;
+    }
+
+    public void setName(@NotEmpty(message = "The name is required") String name) {
+        this.name = name;
+    }
+}
