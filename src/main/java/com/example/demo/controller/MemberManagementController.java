c47c9eaeab285d326656ca620597baf5464a7a8c
phuongnguyenthu <thuphuonggg135@gmail.com>
2024-11-05 14:08:46 +0700

    Merge branch 'refs/heads/ThuPhuong'
    # Conflicts:
    #	src/main/java/com/example/demo/model/Jobtype.java

--- src/main/java/com/example/demo/controller/MemberManagementController.java
+++ src/main/java/com/example/demo/controller/MemberManagementController.java
@@ -13,6 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,7 +24,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @CrossOrigin("*")
 @RequestMapping("/user/member")
 public class MemberManagementController {
